8d35c22912ef798001782152bb3b4213759b19d0
Masha <marya101204@gmail.com>
2023-09-09 22:30:57 +0300

    add/input new worker

--- Workers Homework/main.cpp
+++ Workers Homework/main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
+#include "workers.h"
 using namespace std;
 
 int main() {
-
+	int N = 5;
+	int last = 0;
+	Worker workers[N];
 }
--- Workers Homework/workers.cpp
+++ Workers Homework/workers.cpp
@@ -1,3 +1,20 @@
 #include "workers.h"
 #include <iostream>
 using namespace std;
+
+void inputNewWorker(Worker* worker) {
+	worker->firstName = check("first name");
+	worker->lastName = check("last name");
+	worker->telephone = check("telephone");
+	// worker->salary = check2("salary");
+
+}
+
+Worker* addWorker(Worker workers[], int& last)
+{
+	Worker* worker = new Worker;
+	inputNewWorker(worker);
+	workers[last] = *worker;
+	last++;
+	return worker;
+}
